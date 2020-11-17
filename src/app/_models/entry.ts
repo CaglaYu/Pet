79912dd67f8ce89f630249649c80e9179366406a
CaglaYu <60614778+CaglaYu@users.noreply.github.com>
2020-11-17 18:19:47 +0100

    entry

---
+++ src/app/_models/entry.ts
@@ -0,0 +1,19 @@
+﻿export class Entry {
+    id: string;
+    animaltype: string;
+    name:  string;
+    breed: number;
+    sex: number;
+    age:number;
+    microchip:boolean;
+    location:  string;
+    castrated: boolean;
+    color:  string;
+    height: number;
+    weight: number;
+    image1: ImageData;
+    image2: ImageData;
+    image3: ImageData;
+    lastseen: Date;
+    described:  string;
+}
